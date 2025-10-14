a2e111a6a975d0902b4dd849264f77750ffb01e1
Rafael Ramos Llanos <dr0573131@gmail.com>
2025-10-14 19:53:11 -0300

    a

--- app/models/models.py
+++ app/models/models.py
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(100))
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -18,11 +19,17 @@ class User(UserMixin, db.Model):
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     plate = db.Column(db.String(10), nullable=False)
-    type = db.Column(db.String(10), nullable=False)  # 'car' or 'motorcycle'
-    entry_time = db.Column(db.DateTime, default=datetime.utcnow)
-    exit_time = db.Column(db.DateTime)
-    qr_code = db.Column(db.String(200))
+    type = db.Column(db.String(10), nullable=False)  # 'auto' or 'moto'
+    entry_time = db.Column(db.DateTime, default=datetime.now)
+    exit_time = db.Column(db.DateTime, nullable=True)
+    qr_code = db.Column(db.Text, nullable=True)  # Base64 del QR
     is_monthly = db.Column(db.Boolean, default=False)
+    total_cost = db.Column(db.Float, default=0.0)
+    operator_name = db.Column(db.String(64))  # Operador que registró ingreso
+    exit_operator_name = db.Column(db.String(64))  # Operador que registró salida
+    
+    def __repr__(self):
+        return f'<Vehicle {self.plate}>'
 
 class MonthlyClient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,7 +37,14 @@ class MonthlyClient(db.Model):
     model = db.Column(db.String(50))
     phone = db.Column(db.String(20))
     expiration_date = db.Column(db.DateTime, nullable=False)
-    vehicle_type = db.Column(db.String(10), nullable=False)  # 'car' or 'motorcycle'
+    vehicle_type = db.Column(db.String(10), nullable=False)  # 'auto' or 'moto'
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    
+    def is_expired(self):
+        return self.expiration_date < datetime.now()
+    
+    def __repr__(self):
+        return f'<MonthlyClient {self.plate}>'
 
 @login_manager.user_loader
 def load_user(id):
